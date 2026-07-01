65e63f3431615eec723984b4d3d6b7cee2c86597
rizwangourysk <rizwangourysk@gmail.com>
2026-07-01 22:43:28 +0530

    Initial commit

--- ec2.tf
+++ ec2.tf
@@ -58,11 +58,11 @@ resource aws_security_group my_security_group {
 resource "aws_instance" "my_instance" {
     key_name  = aws_key_pair.my_key.key_name
     security_groups = [aws_security_group.my_security_group.name]
-    instance_type = "t3.micro"
-    ami = "ami-0199ac7c9fbf9ed83" # ubuntu
+    instance_type = var.ec2_instance_type
+    ami = var.ec2_ami_id
 
     root_block_device {
-        volume_size = 15
+        volume_size = var.ec2_root-storage_size
         volume_type = "gp3"
     }
     tags = {
